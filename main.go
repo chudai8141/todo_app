7b362d9d4687bbf9470ff2f8d214923c84bae6a7
chudai <chu.dai.kimukimu.0420@gmail.com>
2022-03-07 14:58:57 +0900

    create app/models/todos.go. and ran main.go using todos.go

--- main.go
+++ main.go
@@ -6,39 +6,38 @@ import (
 )
 
 func main() {
-	/*
-		fmt.Println(config.Config.Port)
-		fmt.Println(config.Config.SQLDriver)
-		fmt.Println(config.Config.DbName)
-		fmt.Println(config.Config.LogFile)
-
-		log.Println("test")
-	*/
-
 	fmt.Println(models.Db)
 
-	/*
-		u := &models.User{}
-		u.Name = "test"
-		u.Email = "test@exmaple.com"
-		u.PassWord = "testtest"
-		fmt.Println(u)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
 
-		u.CreateUser()
-	*/
+	// log.Println("test")
 
-	u, _ := models.GetUser(1)
-
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-
-	u, _ = models.GetUser(1)
-
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@exmaple.com"
+	u.PassWord = "testtest"
 	fmt.Println(u)
 
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	u.CreateUser()
+
+	user, _ := models.GetUser(1)
+	user.CreateTodo("First Todo")
+
+	// u, _ := models.GetUser(1)
+
+	// u.Name = "Test2"
+	// u.Email = "test2@example.com"
+	// u.UpdateUser()
+
+	// u, _ = models.GetUser(1)
+
+	// fmt.Println(u)
+
+	// u.DeleteUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
 
 }
