ccc9b924a4bd8b031e4144f1ec2d131b4b6ccdc1
chudai <chu.dai.kimukimu.0420@gmail.com>
2022-04-02 21:41:07 +0900

    #18 fix bag for base

--- app/models/base.go
+++ app/models/base.go
@@ -29,7 +29,7 @@ func init() {
 	url := os.Getenv("DATABASE_URL")
 	connection, _ := pq.ParseURL(url)
 	connection += "sslmode=require"
-	Db, err := sql.Open(config.Config.SQLDriver, connection)
+	Db, err = sql.Open(config.Config.SQLDriver, connection)
 	if err != nil {
 		log.Fatalln(err)
 	}
